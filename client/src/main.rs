2a505873bb92e2b3821b8773cd2592f4baab8df2
Nathanael TO <nathanaelto.pro@gmail.com>
2022-06-29 14:45:34 +0200

    Waiting PlayerBoard and clean

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,78 +2,75 @@ extern crate core;
 
 mod recover_secret;
 
-use std::io::{BufWriter, Read, Write};
+use std::io::{Read, Write};
 use std::net::TcpStream;
-use common::models::{Hello, JsonMessage, Subscribe, Welcome};
+use std::thread::sleep;
+use std::time;
+use common::models::{JsonMessage, Subscribe};
 
-//TODO à compléter/améliorer
+struct TcpClient {
+    stream: TcpStream,
+}
+
+impl TcpClient {
+    pub fn new() -> TcpClient {
+        let stream_addr = TcpStream::connect("localhost:7878");
+        let mut stream = match stream_addr {
+            Ok(res) => res,
+            Err(err) => panic!("Cannot connect: {err}")
+        };
+        TcpClient {
+            stream
+        }
+    }
+
+    pub fn sendJsonMessage(&mut self, message: &JsonMessage) -> JsonMessage {
+        let message_json = serde_json::to_string(&message).unwrap();
+        let message_size = message_json.len() as u32;
+        self.stream.write(&message_size.to_be_bytes()).unwrap();
+        self.stream.write_all(&message_json.as_bytes()).unwrap();
+
+        let mut size_response_buffer = [0u8; 4];
+        self.stream.read_exact(&mut size_response_buffer).unwrap();
+        let size: u32 = u32::from_be_bytes(size_response_buffer);
+        let mut response_buffer: Vec<u8> = vec![0; size as usize];
+        self.stream.read_exact(&mut response_buffer).unwrap();
+        let response_as_str = std::str::from_utf8(&response_buffer).unwrap();
+        let response : JsonMessage = serde_json::from_str(&response_as_str).unwrap();
+        response
+    }
+
+    pub fn waiting_message(&mut self) -> JsonMessage {
+        let mut size_response_buffer = [0u8; 4];
+        self.stream.read_exact(&mut size_response_buffer).unwrap();
+
+        let size: u32 = u32::from_be_bytes(size_response_buffer);
+        let mut response_buffer: Vec<u8> = vec![0; size as usize];
+        self.stream.read_exact(&mut response_buffer).unwrap();
+        let response_as_str = std::str::from_utf8(&response_buffer).unwrap();
+        let response : JsonMessage = serde_json::from_str(&response_as_str).unwrap();
+        response
+    }
+}
+
+
 fn main() {
-    let stream_addr = TcpStream::connect("localhost:7878");
-
-    let mut stream = match stream_addr {
-        Ok(res) => res,
-        Err(err) => panic!("Cannot connect: {err}")
-    };
-
-    // let hello = Hello {};
-    // println!("{:?}", hello);
-    // let hello_json = serde_json::to_string(&hello);
-    // println!("{:?}", hello_json);
-
-    // println!("-- String and byte --");
-    // let hello = String::from("Hello");
-    // println!("{}", hello);
-    // println!("{}", hello.len());
-    // let hello_byte = hello.as_bytes();
-    // println!("{:?}", hello_byte);
-    // println!("{}", hello_byte.len());
-    //
-    // println!("-- String and json --");
-    // let hello_json = serde_json::to_string(&hello).unwrap();
-    // let hello_json_size = hello_json.len() as u32;
-    // println!("{}", hello_json);
-    // println!("{}", hello_json_size);
+    let mut tcp_client = TcpClient::new();
 
     println!("-- Hello --");
     let hello = JsonMessage::Hello;
-    let hello_json = serde_json::to_string(&hello).unwrap();
-    let hello_size = hello_json.len() as u32;
-    // println!("{}", hello_json);
-    // println!("{}", hello_size);
-
-    stream.write(&hello_size.to_be_bytes()).unwrap();
-    stream.write_all(&hello_json.as_bytes()).unwrap();
-
-    let mut size_response = [0u8; 4];
-    stream.read_exact(&mut size_response).unwrap();
-
-    let size: u32 = u32::from_be_bytes(size_response);
-    let mut data: Vec<u8> = vec![0; size as usize];
-
-    stream.read_exact(&mut data).unwrap();
-    let message_str = std::str::from_utf8(&data).unwrap();
-    // println!("{:?}", message_str);
-
-    let message: JsonMessage = serde_json::from_str(&message_str).unwrap();
+    let message: JsonMessage = tcp_client.sendJsonMessage(&hello);
     println!("{:?}", message);
 
-    println!("-- Subscripbe --");
+    println!("-- Subscribe --");
 
     let name = String::from("Natha");
     let subscribe = JsonMessage::Subscribe(Subscribe {name});
-    let subscribe_json = serde_json::to_string(&subscribe).unwrap();
-    let subscribe_size = subscribe_json.len() as u32;
-
-    stream.write(&subscribe_size.to_be_bytes()).unwrap();
-    stream.write(&subscribe_json.as_bytes()).unwrap();
-
-    let mut subscribe_response_size_buffer =  [0u8; 4];
-    stream.read_exact(&mut subscribe_response_size_buffer).unwrap();
-    let subscribe_response_size: u32 = u32::from_be_bytes(subscribe_response_size_buffer);
-    let mut subscribe_response_buffer: Vec<u8> = vec![0; subscribe_response_size as usize];
-    stream.read_exact(&mut subscribe_response_buffer).unwrap();
-    let subscribe_response_str = std::str::from_utf8(&subscribe_response_buffer).unwrap();
-    let subscribe_response: JsonMessage = serde_json::from_str(&subscribe_response_str).unwrap();
+    let subscribe_response: JsonMessage = tcp_client.sendJsonMessage(&subscribe);
     println!("{:?}", subscribe_response);
+
+    println!("-- Await PlayerBoard --");
+    let board : JsonMessage = tcp_client.waiting_message();
+    println!("{:?}", board);
 
 }
